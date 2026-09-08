41894f89f588c693431d9e8e432c7cc2d2ac4fc4
TOULCHOAT <evann.toulchoat@gmail.com>
2026-09-08 13:30:28 +0200

    Fix des tests unitaires

--- ClassThali/MiniExcursion.cs
+++ ClassThali/MiniExcursion.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace ClassThali
@@ -54,7 +55,7 @@ namespace ClassThali
         /// <param name="uneDuree">duree de l'etape à ajouter</param>
         public void AjouteEtape(string uneDescription, int uneDuree)
         {
-            Etape lEtape = new Etape(1, uneDescription, uneDuree);
+            Etape lEtape = new Etape(this.lesEtapes.Count + 1, uneDescription, uneDuree);
             this.lesEtapes.Add(lEtape);
         }
 
@@ -65,7 +66,12 @@ namespace ClassThali
         /// <returns></returns>
         public int DonneDureePrevue()
         {
-            return 0;
+            int dureeTotale = 0;
+
+            foreach (Etape etape in this.lesEtapes)
+                dureeTotale += etape.getDureePrevue();
+           
+            return dureeTotale;
         }
 
         /// <summary>
@@ -77,7 +83,7 @@ namespace ClassThali
             int dureeMin = this.DonneDureePrevue();
             int mm = dureeMin / 60;
             int hh = dureeMin % 60;
-            return hh.ToString("00") + ":" + mm.ToString("00");
+            return mm.ToString("00") + ":" + hh.ToString("00");
         }
 
 
